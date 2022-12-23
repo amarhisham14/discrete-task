4c6554ed1f6ca613e326d24baeb2efd579ba23a4
amarhisham14 <121308765+amarhisham14@users.noreply.github.com>
2022-12-23 20:52:25 +0200

    Add files via upload

---
+++ perfect numbers/perfect numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using Perfectnumbers;
+
+
+namespace Perfectnumbers
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int i, j;
+
+            Console.WriteLine("please enter your starting number");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your ending number");
+            int end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("perfect Numbers between {0} and {1} are :", start,end);
+
+
+            for (i = start; i <= end; i++)
+            {
+                int sum = 0;    
+                for (j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum+= j;
+                }
+                if (i == sum)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+
+
+
+    }
+    
+}
---
+++ prime numbers/prime numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using Primenumbers;
+
+
+namespace Primenumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            
+            Console.WriteLine("enter your starting number");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your ending number");
+            int end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prime Numbers between {0} and {1} are :",start,end);
+
+
+            for (i = start; i <= end; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                        break;
+                }
+                if (i == j)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+
+    }
+
+}
